34c0132e91256c34b2070e16de4767cf3dbaaf85
Nhokpro2002 <tuannm@newwave.com.vn>
2025-04-24 14:50:51 +0700

    update

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -23,12 +23,17 @@
           lg="4"
         >
           <v-card
-            @click="handleImageClick(image)"
+            @click="handleImageClick(image.category)"
             max-height="350"
             max-width="400"
           >
-            <v-img :src="image.src"></v-img>
             <v-card-text>{{ image.name }}</v-card-text>
+            <v-img
+              :src="image.src"
+              contain
+              aspect-ratio="1"
+              class="mx-auto"
+            ></v-img>
           </v-card>
         </v-col>
       </v-row>
@@ -82,33 +87,39 @@ export default {
       ads: [
         "https://img.freepik.com/free-psd/black-friday-product-sale-social-media-post-design-template_47987-24560.jpg",
         "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/bcee4e175735195.64c65b0e7f738.jpg",
-        "https://img.freepik.com/premium-psd/gaming-laptop-product-sale-advertising-social-media-post_252779-890.jpg", // Ad 1
+        "https://img.freepik.com/premium-psd/gaming-laptop-product-sale-advertising-social-media-post_252779-890.jpg",
       ],
 
       images: [
         {
           name: "LAPTOP OFFICE",
           src: "https://cdn.hoanghamobile.com/Uploads/2024/04/16/laptop-4.jpg",
+          category: "LAPTOP_OFFICE",
         },
         {
           name: "LAPTOP GAMING",
           src: "https://cdn.tgdd.vn//News/1173869//23-730x408-1.jpg",
+          category: "LAPTOP_GAMING",
         },
         {
           name: "SDD vs HDD",
           src: "https://image.dienthoaivui.com.vn/x,webp,q90/https://dashboard.dienthoaivui.com.vn/uploads/dashboard/editor_upload/dau-hieu-hu-o-cung-ssd-01.jpg",
+          category: "SSD",
         },
         {
           name: "MOUSE",
           src: "https://product.hstatic.net/200000680839/product/81dfwlpheel._ac_sl1500__9bea606b9f464b0e854a942dc5659915_f9c3c0c8c0fa43d593f32dc7cd9e492c.jpg",
+          category: "GAMING_MOUSE",
         },
         {
           name: "KEYBOARD",
           src: "https://vgnlab.com/cdn/shop/articles/7.21green_keyboard.jpg?v=1695377731&width=1500",
+          category: "KEYBOARD",
         },
         {
           name: "SCREEN",
           src: "https://images.thdstatic.com/productImages/42da889e-b286-4405-a60c-a39665d82ff8/svn/vivohome-projector-screens-x002cmjaqf-64_600.jpg",
+          category: "SCREEN",
         },
       ],
     };
--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -9,15 +9,19 @@
                 User Registration
               </v-card-title>
               <v-card-text>
-                <v-form @submit.prevent="submitForm" ref="form">
+                <v-form @submit.prevent="submitForm" ref="form" v-model="valid">
                   <v-text-field
                     label="First Name"
                     v-model="form.firstName"
+                    :rules="form.nameRules"
+                    :counter="15"
                     required
                   ></v-text-field>
                   <v-text-field
                     label="Last Name"
                     v-model="form.lastName"
+                    :rules="form.nameRules"
+                    :counter="15"
                     required
                   ></v-text-field>
                   <v-text-field
@@ -28,17 +32,21 @@
                   <v-text-field
                     label="UserName"
                     v-model="form.userName"
+                    :rules="form.nameRules"
+                    :counter="15"
                     required
                   ></v-text-field>
                   <v-text-field
                     label="Email"
                     v-model="form.email"
+                    :rules="form.emailRules"
                     type="email"
                     required
                   ></v-text-field>
                   <v-text-field
                     label="Password"
                     v-model="form.userPassword"
+                    :rules="form.userPasswordRules"
                     type="password"
                     required
                   ></v-text-field>
@@ -64,12 +72,36 @@ export default defineComponent({
   data() {
     return {
       form: {
+        valid: false,
         firstName: "",
         lastName: "",
-        address: "",
         userName: "",
+        nameRules: [
+          (v: string) => !!v || "Name is required",
+          (v: string) =>
+            v.length <= 15 || "Name must be less than 15 characters",
+          (v: string) => !/\s/.test(v) || "Name can't have spaces ",
+        ],
+        address: "",
         email: "",
+        emailRules: [
+          (v: string) => !!v || "Email is required",
+          (v: string) => /.+@.+/.test(v) || "E-mail must be valid",
+          (v: string) => !/\s/.test(v) || "Email can't have spaces ",
+        ],
         userPassword: "",
+        userPasswordRules: [
+          (v: string) => !!v || "Password is required",
+          (v: string) =>
+            v.length >= 8 || "Password must be at least 8 characters",
+          (v: string) =>
+            /[A-Za-z]/.test(v) || "Password must contain at least one letter",
+          (v: string) =>
+            /\d/.test(v) || "Password must contain at least one number",
+          (v: string) =>
+            /[!@#$%^&*(),.?":{}|<>]/.test(v) ||
+            "Password must contain at least one special character",
+        ],
       } as UserRegisterRequest,
     };
   },
